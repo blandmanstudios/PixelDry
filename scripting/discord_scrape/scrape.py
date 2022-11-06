3a29c08d3bb0e7914f2c1c8ff3fec5108f5a13b3
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2022-11-06 02:17:06 -0500

    Add an ugly but functional script that polls
    It polls for new imaging requests then loops through and gets the
    filenames and urls for each of the stages associated with the image
    request being drawn. Then the next step is to make this one bit more
    reliable and make sure i can grab the final generated image from the
    request

--- scripting/discord_scrape/scrape.py
+++ scripting/discord_scrape/scrape.py
@@ -2,6 +2,8 @@
 
 import requests
 import yaml
+import json
+from time import sleep
 
 
 
@@ -13,10 +15,68 @@ def main():
     headers={
             'Authorization': f'{params["access_token"]}'
     }
-    message_id = '1038669289051410503'
-    channel_id = '1022686221970985084'
-    resp = requests.get(f"{API_ENDPOINT}/channels/{channel_id}/messages?limit=50", headers=headers)
-    print(resp.json())
+    mjn_chan_id = '989268312036896818'
+    found_a_zero = False
+    while not found_a_zero:
+        resp = requests.get(f"{API_ENDPOINT}/channels/{mjn_chan_id}/messages?limit=10", headers=headers)
+        res = resp.json()
+        json_formatted_str = json.dumps(res, indent=4)
+        # print(json_formatted_str)
+        for item in res:
+            if (type(item) is not dict):
+                continue
+            if item['author']['username'] == 'Midjourney Bot':
+                status = item['content'].split(' ')[-2:-1][0]
+                print(status)
+                other_status = item['content'].split(' ')[-1:]
+                if '(' in status and '%)' in status:
+                    # this message is in progress
+                    if '(0%)' in status:
+                        print('foundone')
+                        message_id = item['id']
+                        finished = False
+                        prev_filename = None
+                        prev_url = None
+                        count = 0
+                        while not finished and count < 1000:
+                            resp = requests.get(f"{API_ENDPOINT}/channels/{mjn_chan_id}/messages?around={message_id}&limit=1", headers=headers)
+                            res = resp.json()
+                            json_formatted_str = json.dumps(res, indent=4)
+                            # print(json_formatted_str)
+                            for message in res:
+                                if 'attachments' in message and len(message['attachments']) > 0:
+                                    filename = message['attachments'][0]['filename']
+                                    url = message['attachments'][0]['url']
+                                    if (prev_filename != filename and prev_url != url):
+                                        print(filename)
+                                        print(url)
+                                        prev_filename = filename
+                                        prev_url = url
+                                        # Note, there is some trouble getting
+                                        # the png, sometimes it wont get an
+                                        # image, sometimes it will get the wrong
+                                        # image, its probably because of the
+                                        # message "around" trick i'm doing above
+                                        # we should be doing some checking but
+                                        # also figure out if the message id is
+                                        # changing
+                                        # it does, when they get to the final
+                                        # png image they delete the old message
+                                        # and create a new message with a new
+                                        # message id
+                                        # so i need both, i need to be filtering
+                                        # on content (or on image_id) to make
+                                        # sure I'm looking at the thing i want
+                                        # but also i need to add a new search
+                                        # that will search new messages for
+                                        # my png with the right (either content
+                                        # or) message_id
+                                        if '.png' in filename:
+                                            finished = True
+                                            print(f'requests_completed={count}')
+                                break
+                            count += 1
+        print(0.5)
 
 
 if __name__ == '__main__':
