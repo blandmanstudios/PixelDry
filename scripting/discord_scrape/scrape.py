f503f91a2ea1e8fef8bce1fad3c458a9e7036892
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2022-11-06 13:30:55 -0500

    Finish rough alg for scraping all images in-prog
    This finishes a rough rudamentary algorithm for scraping the discord API
    to find an in-progress render (0%) then making repeated requests at it
    to get the filenames and urls of the images associated with that render
    including all the "in-progress" webp images and the "final" png image
    
    Next steps are to refactor this for reliability so it actually has some
    logical way to keep track of state and add some sode to download and
    save the images (still keeping track of state)

--- scripting/discord_scrape/scrape.py
+++ scripting/discord_scrape/scrape.py
@@ -18,7 +18,7 @@ def main():
     mjn_chan_id = '989268312036896818'
     found_a_zero = False
     while not found_a_zero:
-        resp = requests.get(f"{API_ENDPOINT}/channels/{mjn_chan_id}/messages?limit=10", headers=headers)
+        resp = requests.get(f"{API_ENDPOINT}/channels/{mjn_chan_id}/messages?limit=50", headers=headers)
         res = resp.json()
         json_formatted_str = json.dumps(res, indent=4)
         # print(json_formatted_str)
@@ -38,45 +38,44 @@ def main():
                         prev_filename = None
                         prev_url = None
                         count = 0
+                        this_request_hash = None
                         while not finished and count < 1000:
                             resp = requests.get(f"{API_ENDPOINT}/channels/{mjn_chan_id}/messages?around={message_id}&limit=1", headers=headers)
                             res = resp.json()
                             json_formatted_str = json.dumps(res, indent=4)
                             # print(json_formatted_str)
                             for message in res:
-                                if 'attachments' in message and len(message['attachments']) > 0:
+                                if 'attachments' in message and len(message['attachments']) > 0 and (this_request_hash is None or this_request_hash in message['attachments'][0]['filename']):
                                     filename = message['attachments'][0]['filename']
                                     url = message['attachments'][0]['url']
+                                    this_request_hash = filename.split('_')[-1].split('.')[0]
                                     if (prev_filename != filename and prev_url != url):
                                         print(filename)
                                         print(url)
                                         prev_filename = filename
                                         prev_url = url
-                                        # Note, there is some trouble getting
-                                        # the png, sometimes it wont get an
-                                        # image, sometimes it will get the wrong
-                                        # image, its probably because of the
-                                        # message "around" trick i'm doing above
-                                        # we should be doing some checking but
-                                        # also figure out if the message id is
-                                        # changing
-                                        # it does, when they get to the final
-                                        # png image they delete the old message
-                                        # and create a new message with a new
-                                        # message id
-                                        # so i need both, i need to be filtering
-                                        # on content (or on image_id) to make
-                                        # sure I'm looking at the thing i want
-                                        # but also i need to add a new search
-                                        # that will search new messages for
-                                        # my png with the right (either content
-                                        # or) message_id
                                         if '.png' in filename:
                                             finished = True
                                             print(f'requests_completed={count}')
                                 break
+                            resp = requests.get(f"{API_ENDPOINT}/channels/{mjn_chan_id}/messages?limit=50", headers=headers)
+                            res = resp.json()
+                            json_formatted_str = json.dumps(res, indent=4)
+                            # print(json_formatted_str)
+                            for message in res:
+                                if 'attachments' in message and len(message['attachments']) > 0 and (this_request_hash is not None and this_request_hash in message['attachments'][0]['filename']):
+                                    filename = message['attachments'][0]['filename']
+                                    url = message['attachments'][0]['url']
+                                    print(filename)
+                                    print(url)
+                                    if '.png' in filename:
+                                        finished = True
+                                        print('FOUND THE PNG')
+                            print('inner-sleep')
+                            sleep(3)
                             count += 1
-        print(0.5)
+        print('outer-sleep')
+        sleep(3)
 
 
 if __name__ == '__main__':
