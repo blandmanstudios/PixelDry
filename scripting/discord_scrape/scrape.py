9d32d9b267c272c546a88e1d38d02bbc96ecc7c8
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2022-11-06 01:58:15 -0400

    Add ability to query discord (using session token)

--- scripting/discord_scrape/scrape.py
+++ scripting/discord_scrape/scrape.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 import requests
+import yaml
+
+
+
+API_ENDPOINT = 'https://discord.com/api/v10'
 
 def main():
-    print(requests.get)
-    print('hi main')
+    with open('secure_params.yml', 'r') as file:
+        params = yaml.safe_load(file)
+    headers={
+            'Authorization': f'{params["access_token"]}'
+    }
+    message_id = '1038669289051410503'
+    channel_id = '1022686221970985084'
+    resp = requests.get(f"{API_ENDPOINT}/channels/{channel_id}/messages?limit=50", headers=headers)
+    print(resp.json())
 
 
 if __name__ == '__main__':
