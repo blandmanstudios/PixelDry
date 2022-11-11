9738e0e492c0988bd3f087973971f66972f79b39
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2022-11-10 21:31:26 -0500

    Get orchestrate ready to run in prod, new interval
    New settings are run indefinitely and only restart on the hour

--- scripting/discord_scrape/orchestrate.py
+++ scripting/discord_scrape/orchestrate.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from datetime import timedelta
 import argparse
 
-TIME_STEP = 60
+TIME_STEP = 60 * 60
 
 def main():
     parser = argparse.ArgumentParser(prog='myprogram')
@@ -23,12 +23,12 @@ def main():
     begin = datetime.utcnow()
 
     commands = dict(
-        find1={'cmd': ["./find_new_renders.py", "-c", "24", "-i","10"], 'cwd': "."},
-        find2={'cmd': ["./find_new_renders.py", "-c", "54", "-i","10"], 'cwd': "."},
-        find3={'cmd': ["./find_new_renders.py", "-c", "84", "-i","10"], 'cwd': "."},
-        track1={'cmd': ["./track_new_renders.py", "-i","40"], 'cwd': "."},
-        down1={'cmd': ["./download_images.py", "-i","10"], 'cwd': "."},
-        render1={'cmd': ["./render_clips.py", "-i","10"], 'cwd': "."},
+        find1={'cmd': ["./find_new_renders.py", "-c", "24", "-i","-1"], 'cwd': "."},
+        find2={'cmd': ["./find_new_renders.py", "-c", "54", "-i","-1"], 'cwd': "."},
+        find3={'cmd': ["./find_new_renders.py", "-c", "84", "-i","-1"], 'cwd': "."},
+        track1={'cmd': ["./track_new_renders.py", "-i","-1"], 'cwd': "."},
+        down1={'cmd': ["./download_images.py", "-i","-1"], 'cwd': "."},
+        render1={'cmd': ["./render_clips.py", "-i","-1"], 'cwd': "."},
     )
     for key in commands:
         commands[key]["proc"] = subprocess.Popen(commands[key]["cmd"], cwd=commands[key]["cwd"], stdout=subprocess.DEVNULL)
@@ -42,18 +42,16 @@ def main():
         diff = loop_now - prev_time
         diff_seconds = diff.total_seconds()
         if diff_seconds > TIME_STEP:
+            print('an hour passed, rerunning scripts that died')
             for key in commands:
                 if(commands[key]["proc"].poll() is not None):
-                    commands[key]["proc"] = subprocess.Popen(commands[key]["cmd"], cwd=commands[key]["cwd"], stdout=subprocess.DEVNULL)
                     print(f'{key} died, restarting')
-
-
-            print((loop_now - begin).total_seconds())
-            print('a minute passed')
+                    commands[key]["proc"] = subprocess.Popen(commands[key]["cmd"], cwd=commands[key]["cwd"], stdout=subprocess.DEVNULL)
+            print(f'{(loop_now - begin).total_seconds()} seconds passed')
             prev_time += timedelta(seconds=TIME_STEP)
 
 
-        print(i)
+        print(f'loop_count {i}')
         sleep(1)
         i += 1
 
