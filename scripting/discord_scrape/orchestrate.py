5ded352ccc5435f37230f9c9440e47a4476eb0b0
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2022-11-10 21:13:47 -0500

    Add orchestration script
    Right now it has gentle restart for the stuff using the discord API the
    current example runs for 10 seconds and restarts every 60, eventually
    i'll have it run forever and restart every hour

---
+++ scripting/discord_scrape/orchestrate.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import requests
+import yaml
+import json
+from time import sleep
+import sqlite3
+import subprocess
+from datetime import datetime
+from datetime import timedelta
+import argparse
+
+TIME_STEP = 60
+
+def main():
+    parser = argparse.ArgumentParser(prog='myprogram')
+    parser.add_argument('--iterations',
+                        '-i',
+                        type=int,
+                        default=-1,
+                        help='number of loop iterations, -1 for infinite')
+    args = parser.parse_args()
+    begin = datetime.utcnow()
+
+    commands = dict(
+        find1={'cmd': ["./find_new_renders.py", "-c", "24", "-i","10"], 'cwd': "."},
+        find2={'cmd': ["./find_new_renders.py", "-c", "54", "-i","10"], 'cwd': "."},
+        find3={'cmd': ["./find_new_renders.py", "-c", "84", "-i","10"], 'cwd': "."},
+        track1={'cmd': ["./track_new_renders.py", "-i","40"], 'cwd': "."},
+        down1={'cmd': ["./download_images.py", "-i","10"], 'cwd': "."},
+    )
+    for key in commands:
+        commands[key]["proc"] = subprocess.Popen(commands[key]["cmd"], cwd=commands[key]["cwd"], stdout=subprocess.DEVNULL)
+        print(commands[key]["proc"].poll())
+
+
+    prev_time = begin
+    i = 0
+    while args.iterations < 0 or i < args.iterations:
+        loop_now = datetime.utcnow()
+        diff = loop_now - prev_time
+        diff_seconds = diff.total_seconds()
+        if diff_seconds > TIME_STEP:
+            for key in commands:
+                if(commands[key]["proc"].poll() is not None):
+                    commands[key]["proc"] = subprocess.Popen(commands[key]["cmd"], cwd=commands[key]["cwd"], stdout=subprocess.DEVNULL)
+                    print(f'{key} died, restarting')
+
+
+            print((loop_now - begin).total_seconds())
+            print('a minute passed')
+            prev_time += timedelta(seconds=TIME_STEP)
+
+
+        print(i)
+        sleep(1)
+        i += 1
+
+    return
+
+
+if __name__ == '__main__':
+    main()
