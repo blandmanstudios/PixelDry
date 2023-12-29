04857b32fdecf1ec85759367b59d2eec58f2afae
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2023-12-28 23:16:11 -0500

    phase02: add some notes on the design pattern

--- src/wcdraw/phase02_track_progress.py
+++ src/wcdraw/phase02_track_progress.py
@@ -50,15 +50,26 @@ def callback(ch, method, properties, body):
     print(f" [x] Received {body.decode()}")
     time.sleep(body.count(b"."))
     print(" [x] Done")
+    # as soon as the message comes in query it to see if we have a new image URL
+    # if the stage is a finished product
+    #    save the url to the output queue, so someone can curl it
+    #    save the stage info to MySQL
+    #    ack the message
+    # if the stage is 0-99% and has an image URL and we havent seen it before
+    #    save the url to the output queue, so someone can curl it
+    #    save the stage info to MySQL
+    #    schedule a new input task to be requeued to remind me to re-track this
+    #      in 10 seconds (incrementing the count)
+    #    ack the message
+    # if the stage is an image URL we have seen before (i just looked at it)
+    #    save nothing to mysql or the output queu
+    #    scheudle an input task to remind me to check this in 5 seconds
+    #      incrementing the count
+    #    ack the message
+    # if this prompt has been tracked/queried more than 100 times
+    #    save nothing to mysql, dont schedule any input tasks
+    #    ack the message (it will never be processed again)
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    # channel.basic_publish(
-    #    exchange="",
-    #    routing_key="wcd_prompts",
-    #    body=body,
-    #    properties=pika.BasicProperties(
-    #        delivery_mode=pika.DeliveryMode.Persistent
-    #    ),
-    # )
 
 
 if __name__ == "__main__":
