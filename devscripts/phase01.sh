903343775ef6db9d8d5ec68452ef079e86fe55fe
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2023-12-28 20:22:19 -0500

    phase01.sh: Add a new devscript to let phase1 run

---
+++ devscripts/phase01.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# ./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i 1
+# ./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i 20
+
+# Run forever
+./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i -1
