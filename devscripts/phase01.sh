3d8ddcde4e52901aab5acdc1415b0280d8286e07
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-01 16:00:24 -0500

    phase01.sh: Modify script to rerun and log errors

--- devscripts/phase01.sh
+++ devscripts/phase01.sh
@@ -4,4 +4,13 @@
 # ./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i 20
 
 # Run forever
-./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i -1
+# ./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i -1
+
+
+for i in {1..200}
+do
+	echo "Run $i"
+	echo "Run $i" >> phase01_output.txt
+	./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i -1 >> phase01_output.txt 2>&1
+	echo -e "\n\n" >> phase01_output.txt
+done
