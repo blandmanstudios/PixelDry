52717ea3e7058120e4b76f02598294480a40df3c
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-01 20:02:39 -0500

    Add python black formatting to tests

--- devscripts/format_code.sh
+++ devscripts/format_code.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
 black src/wcdraw -l 79
+black src/tests -l 79
--- src/tests/random_tests.py
+++ src/tests/random_tests.py
@@ -4,10 +4,11 @@ from wcdraw.common import timestring_to_datetime, Prompt, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
+
 class TestMethods(unittest.TestCase):
     def test_characters_to_mysql(self):
         # Test inserting non-ascii unicode characters to ensure db is configured correctly
-        special_string = 'ﺖﻳ  and 设 and sław and ך'
+        special_string = "ﺖﻳ  and 设 and sław and ך"
         engine = create_engine(
             f"mariadb+pymysql://testuser:testpassword@localhost/wcddbtest?charset=utf8mb4",
         )
@@ -19,19 +20,19 @@ class TestMethods(unittest.TestCase):
             author_id="doesntmatter",
             author_username=special_string[-4:] + "author",
             author_discriminator="0",
-            timestamp=timestring_to_datetime('2023-12-30T02:04:29.52+00:00'),
-            message_id='doesntmatter',
-            channel_id='doesntmatter'
+            timestamp=timestring_to_datetime("2023-12-30T02:04:29.52+00:00"),
+            message_id="doesntmatter",
+            channel_id="doesntmatter",
         )
         session.add(prompt)
         session.commit()
 
     def test_strings_to_timestamps(self):
-        typical_time = '2023-12-30T02:04:29.52+00:00'
-        atypical_time = '2023-12-30T02:04:29+00:00'
+        typical_time = "2023-12-30T02:04:29.52+00:00"
+        atypical_time = "2023-12-30T02:04:29+00:00"
         timestamp = timestring_to_datetime(typical_time)
         timestamp = timestring_to_datetime(atypical_time)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
