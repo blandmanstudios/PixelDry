5d31afa3d794dab76020882fdc93a7b0b365dd28
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-01 20:01:22 -0500

    Add a phase02 dev loop to allow overnight testing

--- devscripts/phase02.sh
+++ devscripts/phase02.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
-./src/wcdraw/phase02_track_progress.py -c secure_params.yml -i -1
+# run once forever
+# ./src/wcdraw/phase02_track_progress.py -c secure_params.yml -i -1
+
+# Run on repeat logging crashes to a specific file
+for i in {1..200}
+do
+	echo "Run $i"
+	echo "Run $i" >> phase02_output.txt
+	./src/wcdraw/phase02_track_progress.py -c secure_params.yml -i -1 >> phase02_output.txt 2>&1
+	echo -e "\n\n" >> phase02_output.txt
+done
