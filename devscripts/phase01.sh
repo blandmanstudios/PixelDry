d8ffb9476b6c4dae37bbf995cbf7ac493d88af34
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-01 20:00:07 -0500

    phase01: Add a few more channels to query

--- devscripts/phase01.sh
+++ devscripts/phase01.sh
@@ -11,6 +11,6 @@ for i in {1..200}
 do
 	echo "Run $i"
 	echo "Run $i" >> phase01_output.txt
-	./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 -i -1 >> phase01_output.txt 2>&1
+	./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 8 18 -i -1 >> phase01_output.txt 2>&1
 	echo -e "\n\n" >> phase01_output.txt
 done
