b0e13b5e7cdf64c02d9fb441ec2002e8071f3491
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-06 17:55:06 -0500

    Devscripts: Add a little timing info for crash patterns

--- devscripts/phase01.sh
+++ devscripts/phase01.sh
@@ -9,8 +9,8 @@
 # Run on repeat logging crashes to a specific file
 for i in {1..30}
 do
-	echo "Run $i"
-	echo "Run $i" >> phase01_output.txt
+	echo "Run $i $(date)"
+	echo "Run $i $(date)" >> phase01_output.txt
 	./src/wcdraw/phase01_find_prompts.py -c secure_params.yml -l 24 54 84 8 18 -i -1 >> phase01_output.txt 2>&1
 	echo -e "\n\n" >> phase01_output.txt
 done
--- devscripts/phase02.sh
+++ devscripts/phase02.sh
@@ -6,8 +6,8 @@
 # Run on repeat logging crashes to a specific file
 for i in {1..30}
 do
-	echo "Run $i"
-	echo "Run $i" >> phase02_output.txt
+	echo "Run $i $(date)"
+	echo "Run $i $(date)" >> phase02_output.txt
 	./src/wcdraw/phase02_track_progress.py -c secure_params.yml -i -1 >> phase02_output.txt 2>&1
 	echo -e "\n\n" >> phase02_output.txt
 done
