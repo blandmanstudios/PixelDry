f302e79d474591427e7650e66c3703f8410f75b2
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-14 15:01:27 -0500

    Phase03: Refactor for organization/testability
    Break down the phase 3 rendering code down into discrete stages that can
    happend for different videos in parallel. Program execution will be much
    easier to track during debugging. This also abstracted some code into
    functions which can be tested independently. Also this sperated the
    sequencing code from the prep so the sequence can be tuned later as
    necessary

--- src/wcdraw/phase03_render_clips.py
+++ src/wcdraw/phase03_render_clips.py
@@ -52,159 +52,102 @@ def main():
 
 
 def main_loop_iteration(engine):
-    prompt_ids_to_prep = get_top_n_prompt_ids(engine, 5, False)
-    print(prompt_ids_to_prep)
-    for prompt_id in prompt_ids_to_prep:
-        output_file_path = f"outdir/{prompt_id}_video.mp4"
-        print(output_file_path)
-        if os.path.isfile(output_file_path):
-            # if desired output file exists we dont need to recreate it
-            continue
+    prompt_ids = get_top_n_prompt_ids(engine, 5, False)
+    print(prompt_ids)
 
-        # Find the filenames for the render stages and stuff
-        final_url = None
-        prompt_text = ""
-        stage_images = []
-        with Session(engine) as session:
-            q = (
-                session.query(Prompt, RenderStage)
-                .join(RenderStage)
-                .filter(Prompt.id == prompt_id)
-            )
-            for prompt, stage in q.all():
-                final_url = prompt.final_url
-                prompt_text = prompt.prompt_text
-                percentage = stage.percentage
-                extension = "webp"
-                local_path = f"data/{prompt_id}_{percentage}.{extension}"
-                if stage.local_path is None or not os.path.isfile(local_path):
-                    # if there is no file or it is missing lets skip it
-                    # downloading is unreliable and if I implement a cleanup
-                    #   we can do it at the same time
-                    continue
-                print(stage.image_url)
-                stage_images.append(local_path)
-        print(stage_images)
+    prompt_info_arr = get_info_on_prompts(prompt_ids, engine)
 
+    # make the working directory for each prompt
+    for item in prompt_info_arr:
+        item["workdir"] = f"workdir/prompt_{item['prompt_id']}"
+        if not os.path.exists(item["workdir"]):
+            os.mkdir(item["workdir"])
+
+    # Download, format, and convert each of the final images
+    for item in prompt_info_arr:
         # Download the final image
-        local_path = f"workdir/final_image.png"
+        item["final_image_path"] = f"{item['workdir']}/final_image.png"
         # Download the image
-        response = requests.get(final_url, stream=True)
-        with open(local_path, "wb") as outfile:
+        response = requests.get(item["final_url"], stream=True)
+        with open(item["final_image_path"], "wb") as outfile:
             shutil.copyfileobj(response.raw, outfile)
-        print(f"downloaded {final_url} to {local_path}")
-
-        # Create an image which is the annotated final render
-        img = Image.open(local_path).convert("RGBA")
-        myFont = ImageFont.truetype("FreeMonoBold.ttf", 20)
-        img_width_px, img_height_px = img.size
-        shadow_overlay = Image.new("RGBA", img.size, (255, 255, 255, 0))
-        text_overlay = Image.new("RGBA", img.size, (255, 255, 255, 0))
-        text_i3 = ImageDraw.Draw(text_overlay)
-        shadow_i3 = ImageDraw.Draw(shadow_overlay)
-        margin = offset = 10
-        num_lines = 0
-        char_width_px, char_height_px = myFont.getsize("0")
-        max_line_length = math.floor((img_width_px - 10 - 10) / char_width_px)
-        for line in textwrap.wrap(prompt_text, width=max_line_length):
-            text_i3.text((margin, offset), line, font=myFont, fill="#000000")
-            offset += char_height_px
-            num_lines += 1
-        shadow_i3.rectangle(
-            (
-                10,
-                10,
-                img_width_px - 10,
-                10 + (num_lines * myFont.getsize(line)[1]),
-            ),
-            fill=(255, 255, 255, 100),
+        convert_png_to_webp(
+            item["workdir"], "final_image.png", "final_image.webp"
         )
-        out = Image.alpha_composite(img, shadow_overlay)
-        out = Image.alpha_composite(out, text_overlay)
-        out = resize_to_aspect(out, 1920, 1080)
-        out.save(f"workdir/annotated_image.png")
+        item["final_image_path"] = f"{item['workdir']}/final_image.webp"
+        # Resize adding margin as necessary
+        resize_file_in_place(item["final_image_path"], 1920, 1080)
 
-        # Resize and reformat all the progression images so they match
-        for i, item in enumerate(stage_images):
-            fname = item.split("/")[1]
-            shutil.copy(item, "workdir/" + fname)
-            resize_file_in_place("workdir/" + fname, 1920, 1080)
+    # Create an annotated final product image with the right size and filetype
+    for item in prompt_info_arr:
+        item["annotated_image_path"] = f"{item['workdir']}/annotated_image.png"
+        create_annotated_image(
+            item["prompt_text"],
+            item["final_image_path"],
+            item["annotated_image_path"],
+        )
+        convert_png_to_webp(
+            item["workdir"], "annotated_image.png", "annotated_image.webp"
+        )
+        item[
+            "annotated_image_path"
+        ] = f"{item['workdir']}/annotated_image.webp"
 
-        # copy some of the files in multiple times with funny index math to
-        #   make the ending file the right length
-        if len(stage_images) > 16:
-            # cap the number of stages at 16 (ive seen 18)
-            stage_images = stage_images[-16:]
+    # Copy all render stages to the working directory
+    for item in prompt_info_arr:
+        item["working_stage_paths"] = []
+        for i, source_stage_path in enumerate(item["source_stage_paths"]):
+            working_stage_path = f"{item['workdir']}/stage_{i}.webp"
+            item["working_stage_paths"].append(working_stage_path)
+            shutil.copy(source_stage_path, working_stage_path)
+
+    # Resize all the render stage images (adding margin as necessary)
+    for item in prompt_info_arr:
+        for working_stage_path in item["working_stage_paths"]:
+            resize_file_in_place(working_stage_path, 1920, 1080)
+
+    # Arrange the images in order they should render (1 per second)
+    for item in prompt_info_arr:
+        usable_stages = item["working_stage_paths"][-16:]
         index = 0
-        for i, item in enumerate(stage_images):
-            fname = "workdir/seq_%02d.webp" % index
-            shutil.copy(item, fname)
-            resize_file_in_place(fname, 1920, 1080)
+        # first copy in the render stages (dupes as necessary)
+        for stage in usable_stages:
+            shutil.copy(stage, f"%s/seq_%04d.webp" % (item["workdir"], index))
             index += 1
-            if index <= 5 and len(stage_images) < 10:
+            if index <= 5 and len(usable_stages) < 10:
                 # reuse the first few images when we dont have a ton
-                fname = "workdir/seq_%02d.webp" % index
-                shutil.copy(item, fname)
-                resize_file_in_place(fname, 1920, 1080)
+                shutil.copy(
+                    stage, f"%s/seq_%04d.webp" % (item["workdir"], index)
+                )
                 index += 1
-        # Copy the final image in at least twice
-        fname = "seq_%02d.webp" % index
-        proc = subprocess.Popen(
-            [
-                "ffmpeg",
-                "-y",
-                "-i",
-                "final_image.png",
-                "-c:v",
-                "libwebp",
-                fname,
-            ],
-            cwd=f"workdir",
+        # Copy the final image twice
+        shutil.copy(
+            item["final_image_path"],
+            f"%s/seq_%04d.webp" % (item["workdir"], index),
         )
-        proc.communicate()
-        resize_file_in_place("workdir/" + fname, 1920, 1080)
         index += 1
-        fname = "seq_%02d.webp" % index
-        proc = subprocess.Popen(
-            [
-                "ffmpeg",
-                "-y",
-                "-i",
-                "final_image.png",
-                "-c:v",
-                "libwebp",
-                fname,
-            ],
-            cwd=f"workdir",
+        shutil.copy(
+            item["final_image_path"],
+            f"%s/seq_%04d.webp" % (item["workdir"], index),
         )
-        proc.communicate()
-        resize_file_in_place("workdir/" + fname, 1920, 1080)
         index += 1
-        # For the remainder of the 20 seconds copy the annotated image
+        # Fill the rest of the video with the annotated image
         while index <= 19:
-            fname = "seq_%02d.webp" % index
-            proc = subprocess.Popen(
-                [
-                    "ffmpeg",
-                    "-y",
-                    "-i",
-                    "annotated_image.png",
-                    "-c:v",
-                    "libwebp",
-                    fname,
-                ],
-                cwd=f"workdir",
+            shutil.copy(
+                item["annotated_image_path"],
+                f"%s/seq_%04d.webp" % (item["workdir"], index),
             )
-            proc.communicate()
-            resize_file_in_place("workdir/" + fname, 1920, 1080)
             index += 1
-        # Copy in a black frame once
-        fname = "workdir/seq_%02d.webp" % index
-        shutil.copy(f"scripting/discord_scrape/black_frame.webp", fname)
-        resize_file_in_place(fname, 1920, 1080)
+        # Copy in one black frame to end the video
+        shutil.copy(
+            "scripting/discord_scrape/black_frame.webp",
+            f"%s/seq_%04d.webp" % (item["workdir"], index),
+        )
         index += 1
 
-        # render them each into an output mp4 video
+    # Render each of the image sequences into an mp4 video
+    for item in prompt_info_arr:
+        item["output_video_path"] = f"{item['workdir']}/output.mp4"
         cmd = " ".join(
             [
                 "ffmpeg",
@@ -230,25 +173,101 @@ def main_loop_iteration(engine):
                 f"output.mp4",
             ]
         )
-        os.system(f"cd workdir; {cmd}")
+        os.system(f"cd {item['workdir']}; {cmd}")
 
-        # move it off to a finished stuff folder
-        shutil.move("workdir/output.mp4", f"outdir/{prompt_id}_output.mp4")
+    # Move each output video to a destination folder
+    for item in prompt_info_arr:
+        shutil.move(
+            item["output_video_path"],
+            f"outdir/prompt_{item['prompt_id']}_output.mp4",
+        )
+        # TODO -- save the output file path to the database
+        # so this render can be used
 
-        # save into sql that the finished video exists somewhere (and save the
-        #   video render algorithm version number that was used)
-        # clear out the old videos that no longer need to exist now that we
-        #   have a new top10 (or top N) saved on disk somewhere
-        # also clear out old render stage images too (like the one for this
-        #   video) because we wont need it for a while
-        for file in os.listdir("workdir"):
-            os.remove(f"workdir/{file}")
+    # Cleanup by removing the working directories
+    for item in prompt_info_arr:
+        shutil.rmtree(item["workdir"])
 
-        # this algorithm guarentees we'll always have a the topN videos renders
-        #   on disk (not the top N but the topN that are ready)
-        # so the phase4 script can do a query to say of the ones that are
-        #   rendered give topN (and start subbing those into the stream schedule)
+    # TODO -- cleanup rendered videos which are unlikely to be needed soon
     return
+
+
+def get_info_on_prompts(prompt_ids, engine):
+    result_arr = []
+    for prompt_id in prompt_ids:
+        final_url = None
+        prompt_text = None
+        source_stage_paths = []
+
+        with Session(engine) as session:
+            q = (
+                session.query(Prompt, RenderStage)
+                .join(RenderStage)
+                .filter(Prompt.id == prompt_id)
+            )
+            for prompt, stage in q.all():
+                final_url = prompt.final_url
+                prompt_text = prompt.prompt_text
+                if stage.local_path is not None and os.path.isfile(
+                    stage.local_path
+                ):
+                    source_stage_paths.append(stage.local_path)
+        if (
+            prompt_id is not None
+            and final_url is not None
+            and prompt_text is not None
+            and len(source_stage_paths) > 0
+        ):
+            result_arr.append(
+                dict(
+                    prompt_id=prompt_id,
+                    final_url=final_url,
+                    prompt_text=prompt_text,
+                    source_stage_paths=source_stage_paths,
+                )
+            )
+    return result_arr
+
+
+def create_annotated_image(prompt_text, input_path, output_path):
+    # Create an image which is the annotated final render
+    img = Image.open(input_path).convert("RGBA")
+    myFont = ImageFont.truetype("FreeMonoBold.ttf", 20)
+    img_width_px, img_height_px = img.size
+    shadow_overlay = Image.new("RGBA", img.size, (255, 255, 255, 0))
+    text_overlay = Image.new("RGBA", img.size, (255, 255, 255, 0))
+    text_i3 = ImageDraw.Draw(text_overlay)
+    shadow_i3 = ImageDraw.Draw(shadow_overlay)
+    margin = offset = 10
+    num_lines = 0
+    char_width_px, char_height_px = myFont.getsize("0")
+    max_line_length = math.floor((img_width_px - 10 - 10) / char_width_px)
+    for line in textwrap.wrap(prompt_text, width=max_line_length):
+        text_i3.text((margin, offset), line, font=myFont, fill="#000000")
+        offset += char_height_px
+        num_lines += 1
+    shadow_i3.rectangle(
+        (
+            10,
+            10,
+            img_width_px - 10,
+            10 + (num_lines * myFont.getsize(line)[1]),
+        ),
+        fill=(255, 255, 255, 100),
+    )
+    out = Image.alpha_composite(img, shadow_overlay)
+    out = Image.alpha_composite(out, text_overlay)
+    out = resize_to_aspect(out, 1920, 1080)
+    out.save(output_path)
+
+
+def convert_png_to_webp(dirname, infname, outfname):
+    # Convert to .webp
+    proc = subprocess.Popen(
+        ["ffmpeg", "-y", "-i", infname, "-c:v", "libwebp", outfname],
+        cwd=dirname,
+    )
+    proc.communicate()
 
 
 def resize_to_aspect(in_img, desired_width_px, desired_height_px):
