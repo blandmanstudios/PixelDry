f52dbf4f1305eaa3793ceb6de3989ccdc79cbb75
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-14 22:03:03 -0500

    cleanup.sh: add notes on cleaning up phase 3 + 4

--- devscripts/clean_up.sh
+++ devscripts/clean_up.sh
@@ -6,3 +6,17 @@ mariadb --user=$1 --password=$2 wcddb --execute "drop table render_stage; drop t
 rm data/*.webp
 rm phase01_output.txt
 rm phase02_output.txt
+
+
+# To flush the history of what you've streamed in the past do this
+# MySQL
+# delete from render_output_event where id > 0;
+
+
+# To delete all the output renders and evidence that you've rendered them
+# bash
+# rm outdir/prompt_*_output.mp4
+# mysql
+# update prompt set local_video_path=NULL where local_video_path IS NOT NULL;
+
+
