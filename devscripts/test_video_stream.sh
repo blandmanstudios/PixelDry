07d841d2bbe802e4cd8d29d2599875832b2cb22c
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-15 15:19:24 -0500

    Add a helpful comment for testing "offline" streaming
    This alternate command can test running the ffmpeg encoder into a local
    file which can be a helpful test when you want to check that your
    encoding setting are working. This is helpful for isolating network
    issues

--- devscripts/test_video_stream.sh
+++ devscripts/test_video_stream.sh
@@ -7,7 +7,10 @@ export DATE=$(date)
 echo $DATE
 echo $DATE >> date_file_started.txt
 
+# Test to just an output file to verify if network connectivity is the issue
+# ffmpeg -re -stream_loop -1 -f concat -safe 0 -i video_list_test30.txt -stream_loop -1 -f concat -safe 0 -i audio_list.txt -map 0:v -map 1:a -c:v libopenh264 -shortest -qscale 0 -g 1 -f flv outputfile.mp4
 
+# Test the actual output stream (to youtube)
 ffmpeg -re -stream_loop -1 -f concat -safe 0 -i video_list_test30.txt -stream_loop -1 -f concat -safe 0 -i audio_list.txt -map 0:v -map 1:a -c:v libopenh264 -shortest -qscale 0 -g 1 -f flv rtmp://$STREAM_URL/$STREAM_KEY
 
 
