ba69154688aa19012b6e0acd4d31fc70bd1545dc
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-15 23:03:02 -0500

    Devscripts Phase 03 + 04: Add retry and logging

--- devscripts/phase04.sh
+++ devscripts/phase04.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
 
 # run once forever
-./src/wcdraw/phase04_stream.py -c secure_params.yml -i -1
+# ./src/wcdraw/phase04_stream.py -c secure_params.yml -i -1
 
 # run once forever (in dry run mode -> no ffmpeg)
-#./src/wcdraw/phase04_stream.py -c secure_params.yml -i -1 --dry-run
+# ./src/wcdraw/phase04_stream.py -c secure_params.yml -i -1 --dry-run
+#
+# Run on repeat logging crashes to a specific file
+for i in {1..30}
+do
+	echo "Run $i $(date)"
+	echo "Run $i $(date)" >> phase04_output.txt
+	./src/wcdraw/phase04_stream.py -c secure_params.yml -i -1 >> phase04_output.txt 2>&1
+	echo -e "\n\n" >> phase04_output.txt
+done
