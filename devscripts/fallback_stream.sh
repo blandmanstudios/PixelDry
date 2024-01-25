b7d3a4cc8c63b4db9b9abe0682f530448a525e6a
steve_is_bland <6804852-steve_is_bland@users.noreply.gitlab.com>
2024-01-24 21:31:52 -0500

    Modify fallback script to be smoot and use image
    Before this the fallback stream was limited to a super low bitrate so
    the quality was trash
    Also now we have an image to stream when the fallback stream is live

--- devscripts/fallback_stream.sh
+++ devscripts/fallback_stream.sh
@@ -5,7 +5,7 @@ cd outdir
 while :
 do
 	echo "Run the stream"
-	ffmpeg -re -stream_loop 2 -i 't_vid0000.mp4' -stream_loop -1 -i audio0000.mp3 -map 0:v -map 1:a -c:v libopenh264 -shortest -b:v 1k -b:a 128k -qscale 0 -g 90 -f flv rtmp://$STREAM_URL/$STREAM_KEY
+	ffmpeg -re -framerate 1 -stream_loop 40 -i 'technical_difficulties_v1.png' -stream_loop -1 -i audio0000.mp3 -map 0:v -map 1:a -c:v libx264 -r 30 -shortest -b:a 128k -qscale 0 -g 10 -preset fast -crf 38 -f flv rtmp://$STREAM_URL/$STREAM_KEY
 	echo "Sleep for 20 seconds"
 	sleep 20
 done
